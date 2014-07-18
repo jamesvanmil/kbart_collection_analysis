d566d9fc74c2c9ced867e0510570f33d3dd04a64
James Van Mil <james.vanmil@gmail.com>
2014-07-18 13:31:20 -0400

    Add ISSN indexer; closes #1

--- Gemfile
+++ Gemfile
@@ -1,3 +1,3 @@
 source 'https://rubygems.org'
 
-gem "active_sierra_models"
+gem "active_sierra_models", git: "https://github.com/uclibs/active_sierra_models.git", branch: "master"
--- kbart_collection_analysis.rb
+++ kbart_collection_analysis.rb
@@ -1,4 +1,29 @@
 require 'bundler/setup'
 require 'active_sierra_models'
 
-puts BibView.first.title
+def issn_index
+  @issn_hash = Hash.new
+  
+  def hash_setter(bib, issn)
+    issn_match = /^\d{4}-\d{3}[\dXx]$/
+
+    if issn =~ issn_match
+      if @issn_hash.has_key? issn
+        @issn_hash[issn] << bib
+      else
+        @issn_hash[issn] = [bib]
+      end
+
+      @issn_hash[issn].uniq!
+    end
+  end
+
+  issn_fields = VarfieldView.marc_tag("022").record_type_code("b").limit(1000)
+  issn_fields.each do |field| 
+    field.subfields.tag("a").each { |a| hash_setter(field.record_num, a.content) }
+  end
+end
+
+issn_index
+
+puts @issn_hash
