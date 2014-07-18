acc49752bc9cbbe24fadcfcc3f016164e40744c6
James Van Mil <james.vanmil@gmail.com>
2014-07-18 19:18:29 -0400

    In middle of working on volume fetcher

--- kbart_collection_analysis.rb
+++ kbart_collection_analysis.rb
@@ -19,7 +19,7 @@ def issn_index
     end
   end
 
-  issn_fields = VarfieldView.marc_tag("022").record_type_code("b").limit(10)
+  issn_fields = VarfieldView.marc_tag("022").record_type_code("b").limit(1000)
   issn_fields.each do |field| 
     field.subfields.tag("a").each { |a| hash_setter(field.record_num, a.content) }
     field.subfields.tag("y").each { |y| hash_setter(field.record_num, y.content) }
@@ -27,11 +27,15 @@ def issn_index
   end
 end
 
+def get_volumes(bib_number)
+  BibView.where("record_num = ?", bib_number)[0].item_views { |item| puts item.varfield_views.varfield_type_code("v").field_content }
+end
+
 class KBART
   'Accept KBART row and extract useful information - accepts row of CSV data loaded with headers'
-  attr_accessor :title, :issns, :begin_date, :end_date, :url, :collection
+  attr_accessor :title, :issns, :begin_date, :end_date, :url, :collection, :bib_records
 
-  def initialize(row)
+  def initialize(row, hash)
     @title = row.field("publication_title")
     @issns = Array.new
     @issns << row.field("print_identifier") unless row.field("print_identifier").nil?
@@ -40,6 +44,15 @@ class KBART
     @end_date = row.field("date_last_issue_online")
     @url = row.field("title_url")
     @collection = row.field("collection")
+    @bib_records = get_bibs(hash)
+  end
+
+  private
+
+  def get_bibs(hash)
+    array = Array.new
+    self.issns.each { |issn| array.concat hash[issn] if hash.has_key? issn }
+    array.uniq
   end
 end
 
@@ -49,6 +62,10 @@ puts @issn_hash
 kbart = CSV.read(ARGV[0], col_sep: "\t", headers: :first_row)
 
 kbart.each do |row|
-  holding = KBART.new(row)
-  puts holding.title, holding.issns, holding.collection
+  bib_records = Array.new
+  holding = KBART.new(row, @issn_hash)
+
+  next if holding.bib_records.length == 0
+
+  holding.bib_records.each { |bib| get_volumes(bib) }
 end
