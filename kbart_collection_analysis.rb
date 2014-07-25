8c2b9e518b0e324b7c94acc989e23cc2493753ee
James Van Mil <james.vanmil@gmail.com>
2014-07-25 11:58:23 -0400

    Move volume date parsing into Item class; begin work on Month parser

--- kbart_collection_analysis.rb
+++ kbart_collection_analysis.rb
@@ -1,6 +1,7 @@
 require 'bundler/setup'
 require 'active_sierra_models'
 require 'csv'
+require 'date'
 
 def issn_index
   ## Query the Sierra database fo all ISSNs (MARC Tag 022: |a,|y,|l) on all bib records)
@@ -36,36 +37,18 @@ class KBART
     @issns = Array.new
     @issns << row.field("print_identifier") unless row.field("print_identifier").nil?
     @issns << row.field("online_identifier") unless row.field("online_identifier").nil?
-    @begin_date = DateTime.parse(row.field("date_first_issue_online"))
-    @end_date = DateTime.parse(row.field("date_last_issue_online"))
+    @begin_date = DateTime.strptime(row.field("date_first_issue_online"), '%Y-%m-%d')
+    @end_date = DateTime.strptime(row.field("date_last_issue_online"), '%Y-%m-%d')
     @url = row.field("title_url")
     @collection = row.field("collection")
     @bib_records = get_bibs(hash)
   end
 
-  def within_holdings?(volume)
-    ## Accepts item and returns true if the item volume statments are within the holding
-    date_matches =  /\((\d\d)(\d\d)[-\\\/](\d\d)?(\d\d)?\)/.match(volume)
-		return false if date_matches.nil?
-
-    volume_begin = DateTime.parse(date_matches[1] + date_matches[2])
-    
-    if date_matches[3].nil?
-      volume_end = DateTime.parse(date_matches[1] + date_matches[2])
-    elsif date_matches[4].nil?
-      increment = true if date_matches[3].to_i < date_matches[2].to_i
-      if increment
-        volume_end = DateTime.parse((date_matches[1].to_i + 1).to_s + date_matches[3])
-      else
-        volume_end = DateTime.parse(date_matches[1] + date_matches[3])
-      end
-    else
-     volume_end = DateTime.parse(date_matches[3] + date_matches[4])
-    end
-
-    return false if volume_begin < self.begin_date
-    return false if volume_end > self.begin_date
-
+  def within_holdings?(dates)
+    ## Accepts date Array with begin and end dates and returns true if the date statments are within the holding
+    dates.each { |date| return false if date.nil? }
+    return false if dates[0] < self.begin_date
+    return false if dates[1] > self.end_date
     true
   end
 
@@ -80,13 +63,64 @@ end
 
 class Item
   ## Accept ItemView object and create object with all of the information we will need for comparison
-  attr_accessor :item_number, :volumes, :call_numbers, :begin_date, :end_date, :location
+  attr_accessor :item_number, :volume, :call_number, :dates, :location, :status, :supression
 
   def initialize(item_view)
     @item_number = item_view.record_num
-    @volumes = item_view.varfield_views.varfield_type_code("v").collect { |f| f.field_content }
-    @call_numbers = item_view.varfield_views.varfield_type_code("c").collect { |f| f.field_content }
+    @volume = volume_parser(item_view)
+    @call_number = call_number_parser(item_view)
     @location = item_view.location_code
+    @status = item_view.item_status_code
+    @supression = item_view.icode2
+    @dates = date_parser(volume)
+  end
+
+  private
+  
+  def volume_parser(item_view)
+    ## Assume there is only one volume
+    volumes = item_view.varfield_views.varfield_type_code("v").collect { |f| f.field_content }
+    volumes[0]
+  end
+
+  def call_number_parser(item_view)
+    ## Assume there is only one call number
+    call_numbers = item_view.varfield_views.varfield_type_code("c").collect { |f| f.field_content }
+    call_numbers[0]
+  end
+
+  def date_parser(volume)
+    ## Parsing attempt #1: (YYYY), (YYYY/YY), (YYYY/YYYY)
+    date_matches =  /\((\d\d)(\d\d)[-\\\/]?(\d\d)?(\d\d)?\)/.match(volume)
+
+    ## Parsing attemt #2: ^YYYY$, ^YYYY/YY$, ^YYYY/YYYY$
+    date_matches =  /^(\d\d)(\d\d)[-\\\/]?(\d\d)?(\d\d)?$/.match(volume) if date_matches.nil?
+
+    unless date_matches.nil?
+      volume_begin = Date.strptime(date_matches[1] + date_matches[2], '%Y')
+      if date_matches[3].nil?
+        volume_end = Date.strptime(date_matches[1] + date_matches[2], '%Y')
+      elsif date_matches[4].nil?
+        increment = true if date_matches[3].to_i < date_matches[2].to_i
+        if increment
+          volume_end = Date.strptime((date_matches[1].to_i + 1).to_s + date_matches[3], '%Y')
+        else
+          volume_end = Date.strptime(date_matches[1] + date_matches[3], '%Y')
+        end
+      else
+       volume_end = Date.strptime(date_matches[3] + date_matches[4], '%Y')
+      end
+      return [volume_begin, volume_end]
+    end
+
+    ## Parsing attempt #3: (YYYY:Mon.), (YYYY:Mon./Mon.)
+    month_hash = [ "Jan" => "1", "Feb" => "2", "Mar" => "3", 
+    date_matches = /\(\d\d\d\d):([AZaz]+\.?)[-\\\/]?([AZaz]+\.?)?/.match(volume)
+    unless date_matches.nil?
+      volume_begin = Date.strptime(date_matches[1], '%Y')
+      if date_matches.
+    end
+   return [nil, nil] 
   end
 end
 
@@ -101,7 +135,6 @@ kbart.each do |row|
   holding.bib_records.each do |bib_number|
     b = BibView.where("record_num = ?", bib_number).first
 
-    puts b.cataloging_date_gmt
     next if b.cataloging_date_gmt = nil
     next unless b.bcode3 = '-'
 
@@ -109,7 +142,9 @@ kbart.each do |row|
     items = b.item_views.collect { |i| Item.new(i) }
     items.each do |i|
       next unless i.location =~ /^u/
-      puts bib_title, i.item_number, i.volumes
+      next if i.location == 'uint'
+      next unless i.supression == "-"
+      puts "#{holding.within_holdings?(i.dates)}\t#{bib_title}\ti#{i.item_number}a\t#{i.call_number}\t#{i.volume}\t#{holding.collection}\t#{holding.begin_date.year}-#{holding.end_date.year}"
     end
   end
     
